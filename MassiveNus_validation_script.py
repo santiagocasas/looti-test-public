ac3a39813d2c9fdf8ed087af15b9c8fed4841d3b
Baena Raphael <38126139+raphael-baena@users.noreply.github.com>
2020-08-21 10:46:25 +0200

    updated script

--- MassiveNus_validation_script.py
+++ MassiveNus_validation_script.py
@@ -25,15 +25,16 @@ emulation_data = dhl.DataHandle( datafile_ext, outputfolder,datafile_LCDM,
 emulation_data.read_csv_pandas(verbosity=2)
 
 redshift=[k for k in range(0,60)]
-emulation_data.calculate_ratio_by_redshifts(redshift)
+emulation_data.calculate_ratio_by_redshifts(redshift, normalize=False)
 
 linkgrid = emulation_data.lin_k_grid
 mask = [k for k in np.where(linkgrid <8e-4)[0] if k in np.where(linkgrid >1e-4)[0]]
 GLOBAL_applymask = True
 thinning = 1
-max_ntrain = 5
-PCA_dict_cross,PCA_dict_cros_all = av.cross_validation(emulation_data=emulation_data, wanted_ntest=10, n_vali= 10,
-                                 operator="PCA", max_train_size=6,interp_type="GP")
+max_ntrain = 55
+PCA_dict_cross,PCA_dict_cros_all = av.cross_validation(emulation_data=emulation_data, wanted_ntest=10, n_vali= 1,
+                                 operator="DL", max_train_size=max_ntrain,interp_type="GP")
+
 PCA_cross_validation_grouped =av.op_crossval_df_dict_mingroup_function(emulation_data=emulation_data, op_crossval_df_dict=PCA_dict_cross)
 datatest_df_dict=av.RMSE_dictionary(emulation_data,PCA_crossval_df_dict_mingroup=PCA_cross_validation_grouped,wanted_ntest=10,max_ntrain=max_ntrain,turnoff_PCA=False)
 print(datatest_df_dict['theo'].groupby(['n_train']).mean())
