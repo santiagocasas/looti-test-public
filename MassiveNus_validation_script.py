d9d80e13b271d4772f1723bb9a398c52d0dd4641
Baena Raphael <38126139+raphael-baena@users.noreply.github.com>
2020-09-11 16:13:12 +0200

    script

--- MassiveNus_validation_script.py
+++ MassiveNus_validation_script.py
@@ -19,22 +19,24 @@ datafile_LCDM = "Massive_Nus_LCDM"
 outputfolder = "./data/"
 
 emulation_data = dhl.DataHandle( datafile_ext, outputfolder,datafile_LCDM, 
-                                  param_names_dict={'parameter_1':'mnv','parameter_2':'om','parameter_3':'As'},
-                               multindex_cols_ext=[0,1,2,3,4,5,6,7],multindex_cols_ref=[0,1])
+                                  num_parameters=3)
+ 
 
 emulation_data.read_csv_pandas(verbosity=2)
 
-redshift=[k for k in range(0,60)]
-emulation_data.calculate_ratio_by_redshifts(redshift, normalize=False)
+
+emulation_data.calculate_ratio_by_redshifts(emulation_data.z_vals, normalize=False)
 
 linkgrid = emulation_data.lin_k_grid
-mask = [k for k in np.where(linkgrid <8e-4)[0] if k in np.where(linkgrid >1e-4)[0]]
+mask = [k for k in np.where(linkgrid <10)[0] if k in np.where(linkgrid >0.1)[0]]
 GLOBAL_applymask = True
-thinning = 1
-max_ntrain = 55
-PCA_dict_cross,PCA_dict_cros_all = av.cross_validation(emulation_data=emulation_data, wanted_ntest=10, n_vali= 1,
-                                 operator="DL", max_train_size=max_ntrain,interp_type="GP")
 
-PCA_cross_validation_grouped =av.op_crossval_df_dict_mingroup_function(emulation_data=emulation_data, op_crossval_df_dict=PCA_dict_cross)
-datatest_df_dict=av.RMSE_dictionary(emulation_data,PCA_crossval_df_dict_mingroup=PCA_cross_validation_grouped,wanted_ntest=10,max_ntrain=max_ntrain,turnoff_PCA=False)
-print(datatest_df_dict['theo'].groupby(['n_train']).mean())
+
+thinning = 1
+min_ntrain = 2
+max_ntrain = 5
+wanted_ntest = 30
+PCA_dict_cross,PCA_dict_cros_all = av.cross_validation(emulation_data=emulation_data, wanted_ntest=wanted_ntest, n_vali= 1,
+                                 operator="PCA", max_train_size = max_ntrain ,min_train_size=min_ntrain,interp_type="GP",number_of_splits=2)
+
+PCA_dict_cross.to_csv("./PCA_MassiveNus.csv")
